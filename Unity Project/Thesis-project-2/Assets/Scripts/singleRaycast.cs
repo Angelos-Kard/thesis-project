18d31b265991458bbbd39dadb5cee23ad593752f
Angelos Kardoutsos <angelkard77@gmail.com>
2023-09-17 15:39:26 +0300

    GH-18: Renamed function to 'SingleRaycastFunc'

--- Unity Project/Thesis-project-2/Assets/Scripts/singleRaycast.cs
+++ Unity Project/Thesis-project-2/Assets/Scripts/singleRaycast.cs
@@ -6,8 +6,7 @@ public class singleRaycast : MonoBehaviour
 {
     [Tooltip("The object which will operate as an alert box for the specific raycast.")]
     public GameObject alertBox;
-
-    public void singleRaycastFunc()
+    public void SingleRaycastFunc()
     {
         Vector3 fwd = transform.TransformDirection(Vector3.forward); // forward direction
 
