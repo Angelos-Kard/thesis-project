eb2613353b95ed5e969a58b03bf62c649fbbbbde
Angelos Kardoutsos <angelkard77@gmail.com>
2023-09-17 15:41:14 +0300

    GH-25: Created continuous mode
    - With the voice command 'Continuous Mode', the functionality is enabled and disabled

--- Unity Project/Thesis-project-2/Assets/Scripts/singleRaycast.cs
+++ Unity Project/Thesis-project-2/Assets/Scripts/singleRaycast.cs
@@ -6,6 +6,21 @@ public class singleRaycast : MonoBehaviour
 {
     [Tooltip("The object which will operate as an alert box for the specific raycast.")]
     public GameObject alertBox;
+    [Tooltip("If it set to 'True', rays will be casted in every frame.")]
+    public bool continuousRaycast = false;
+
+    public void FixedUpdate()
+    {
+        if (continuousRaycast)
+        {
+            SingleRaycastFunc();
+        }
+    }
+
+    public void ContinuousModeToggle()
+    {
+        continuousRaycast = !continuousRaycast;
+    }
     public void SingleRaycastFunc()
     {
         Vector3 fwd = transform.TransformDirection(Vector3.forward); // forward direction
