d77d0d4d432fde1b5a5a074d867e18d074663695
Angelos Kardoutsos <angelkard77@gmail.com>
2023-12-16 17:55:04 +0200

    Added new features in scanCast script

--- Unity Project/Thesis-project-2/Assets/Scripts/castModes/scanCast.cs
+++ Unity Project/Thesis-project-2/Assets/Scripts/castModes/scanCast.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(initCastAndBoxesHandler))]
+[RequireComponent(typeof(initCastAndBoxesHandler)), RequireComponent(typeof(initBoxes))]
 public class scanCast : MonoBehaviour
 {
     private variablesAggregator variableAggInstance;
@@ -12,14 +12,41 @@ public class scanCast : MonoBehaviour
         variableAggInstance = this.GetComponent<initCastAndBoxesHandler>().variableAggregatorObject.GetComponent<variablesAggregator>();
     }
 
-    // Update is called once per frame
     public void EnableScanMode()
     {
         GameObject[] allCastBoxes = variableAggInstance.raycastBoxes;
+        List<RaycastHit> castHitPointsList = new List<RaycastHit>();
 
         for (int i = 0; i < allCastBoxes.Length; i++)
         {
-            allCastBoxes[i].GetComponent<singleRaycast>().SingleRaycastFunc();
+            RaycastHit hitPoint = allCastBoxes[i].GetComponent<singleRaycast>().SingleRaycastFunc(variableAggInstance);
+            castHitPointsList.Add(hitPoint);
+        }
+
+        //ToArray doesn't change the order of the elements
+        int hitPointIndex = this.GetComponent<initBoxes>().findClosestHitPointIndex(castHitPointsList.ToArray());
+        if (hitPointIndex != -1)
+        {
+            this.GetComponent<initBoxes>().placeAlertBox(variableAggInstance.commonAlertBox, allCastBoxes[hitPointIndex], castHitPointsList[hitPointIndex], variableAggInstance.placeAlertBoxAtEyeLevel);
+        }
+        else
+        {
+            switch (variableAggInstance.alertBoxType)
+            {
+                case variablesAggregator.AlertBoxTypeEnum.BoxSpecific:
+                    // Use the alert box of each castBox
+                    this.GetComponent<initBoxes>().deactivateAlertBox(variableAggInstance.alertBoxes);
+                    break;
+
+                case variablesAggregator.AlertBoxTypeEnum.Common:
+                    // Use a common alert for all castBoxes
+                    this.GetComponent<initBoxes>().deactivateAlertBox(variableAggInstance.commonAlertBox);
+                    break;
+
+                case variablesAggregator.AlertBoxTypeEnum.Peripheral:
+                    this.GetComponent<initBoxes>().deactivateAlertBox(variableAggInstance.peripheralAlertBox);
+                    break;
+            }
         }
     }
 }
